f8dde99dcfd3c903c7f05aad9a86a6c653379253
Navin Fischer <navin.fischer@gmail.com>
2025-07-14 19:28:57 +0200

    Delete restart_order-service.sh

--- restart_order-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t payment-service:latest ./services/payment-service
-
-kind load docker-image payment-service:latest --name mokelumne-cluster
-
-kubectl rollout restart deployment payment-service -n mokelumne
